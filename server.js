6092df10806cb4a3227039dfefcd99581fe5c923
imadtawab <89462211+imadtawab@users.noreply.github.com>
2023-07-18 23:45:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use("/products", ProductsRoutes);
 
 // GET /=
 app.use((req , res) => {
-    res.status(404).json({
+    res.json({
         message: "this page not found",
         status: 404
     })
